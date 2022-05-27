f31bdfeb59568af51e4a439f255eaa99a761410c
Nelson <jhoeladarmes@gmail.com>
2022-05-27 12:08:26 -0300

    Generé una parte del algoritmo de facturación

--- algoritmo.js
+++ algoritmo.js
@@ -6,10 +6,17 @@ let elementosInventario = 1;
 let w = "Esta variable sólo se usa en el bucle para el conteo del intenvario";
 let elemento = "valor booleano";
 
-let cantidadCerveza = "cantidades de cerveza";
-let cantidadCocacola = "cantidades de cocacola";
-let cantidadLevite = "cantidades de levite manzana";
-let cantidadLevitePera = "cantidades de levite pera";
+let cantidadCerveza = 0 //"cantidades de cerveza";
+let cantidadCocacola = 0//"cantidades de cocacola";
+let cantidadLevite = 0//"cantidades de levite manzana";
+let cantidadLevitePera = 0//"cantidades de levite pera";
+let cantidadTotalProductos = 0//"total de la cantidad de productos";
+
+let total_cerveza = 0
+let total_cocacola = 0
+let total_levite = 0
+let total_levitePera = 0
+let total_general = 0
 
 //--Este bucle automatiza el conteo de elementos en el inventario
 do {
@@ -31,56 +38,70 @@ for (i=1 ; i < elementosInventario ; i ++){
 
 function asignarCantidades(){
 
-    cantidadCerveza = document.querySelector("#cantidades_1").value; 
-    cantidadCocacola = document.querySelector("#cantidades_2").value;
-    cantidadLevite = document.querySelector("#cantidades_3").value;
-    cantidadLevitePera = document.querySelector("#cantidades_4").value;
+    cantidadCerveza = Number(document.querySelector("#cantidades_1").value); 
+    cantidadCocacola = Number(document.querySelector("#cantidades_2").value);
+    cantidadLevite = Number(document.querySelector("#cantidades_3").value);
+    cantidadLevitePera = Number(document.querySelector("#cantidades_4").value);
 }
 
 //Este bucle genera los espacios en el carrito de compra según la cantidad de elementos
 for (i=1 ; i < elementosInventario ; i ++){
     document.querySelector(`#carrito_de_compra_total`).innerHTML +=`
-    <h3 id="carrito_de_compra_${i}"></h3>
+    <h6 id="carrito_de_compra_${i}"></h6>
     
     `;
 }
 
 function verCarritoDeCompra (){
+    total_cerveza = 0;
+    total_cocacola = 0;
+    total_levite = 0;
+    total_levitePera = 0;
+    document.querySelector(`#total_a_pagar`).innerHTML = ``;
+
     for (i=1 ; i < elementosInventario ; i ++){
         
-        document.querySelector(`#carrito_de_compra_${i}`).innerHTML=``
+        document.querySelector(`#carrito_de_compra_${i}`).innerHTML=``;
     }
     for (i=1 ; i < elementosInventario ; i ++){
-        let k = document.querySelector(`#cantidades_${i}`).value
+        let k = document.querySelector(`#cantidades_${i}`).value;
             if (i==1 & k > 0 ){
-                let total_cerveza = cantidadCerveza * PRECIO_CERVEZA 
+                total_cerveza = cantidadCerveza * PRECIO_CERVEZA 
                 document.querySelector(`#carrito_de_compra_${i}`).innerHTML +=`
-                    <h3> ${cantidadCerveza} Cerveza Litro $${total_cerveza}</h3><br>
+                    ${cantidadCerveza} Cerveza Litro $${total_cerveza}
                 `
             }
             else if (i==2 & k > 0){
-                let total_cocacola = cantidadCocacola * PRECIO_COCACOLA
+                total_cocacola = cantidadCocacola * PRECIO_COCACOLA
                 document.querySelector(`#carrito_de_compra_${i}`).innerHTML +=`
-                    <h3> ${cantidadCocacola} Coca-cola $${total_cocacola}</h3><br>
+                    ${cantidadCocacola} Coca-cola $${total_cocacola}
                 `
             }    
             else if (i==3 & k > 0){
-                let total_levite = cantidadLevite * PRECIO_LEVITE
+                total_levite = cantidadLevite * PRECIO_LEVITE
                 document.querySelector(`#carrito_de_compra_${i}`).innerHTML +=`
-                    <h3> ${cantidadLevite} Levité Manzana Litro $${total_levite}</h3><br>
+                    ${cantidadLevite} Levité Manzana Litro $${total_levite}
                ` 
             }
             else if (i==4 & k > 0){
-                let total_levitePera = cantidadLevitePera * PRECIO_LEVITE
+                total_levitePera = cantidadLevitePera * PRECIO_LEVITE
                 document.querySelector(`#carrito_de_compra_${i}`).innerHTML +=`
-                    <h3> ${cantidadLevitePera} Levité Pera Litro $${total_levitePera}</h3><br>
+                    ${cantidadLevitePera} Levité Pera Litro $${total_levitePera}
                `
             }    
-            else{};
-    }                    
+            else {};
+    }
+    total_general = 0 + (total_cerveza + total_cocacola + total_levite + total_levitePera)
+                document.querySelector(`#total_a_pagar`).innerHTML =`
+                    <h4> Total= $${total_general}</h4><br>`;                    
 }
 function vaciarCarrito(){
-    
+    total_cerveza = 0;
+    total_cocacola = 0
+    total_levite = 0
+    total_levitePera = 0
+    document.querySelector(`#total_a_pagar`).innerHTML = ``;
+
     for (i=1 ; i < elementosInventario ; i ++){
         
         document.querySelector(`#cantidades_${i}`).innerHTML =``
@@ -96,4 +117,46 @@ function vaciarCarrito(){
     for (i=1 ; i < elementosInventario ; i ++){
         document.querySelector(`#carrito_de_compra_${i}`).innerHTML=``
     }    
+}
+
+function pagoTarjeta(){
+    cantidadTotalProductos = (cantidadCerveza + cantidadCocacola + cantidadLevite + cantidadLevitePera);
+    console.log(cantidadTotalProductos)
+    
+    if(cantidadTotalProductos < 3){
+    
+    document.querySelector("#facturacion").innerHTML=`
+        
+        <label>Cantidad de cuotas
+            <select>
+                <option>1 cuota </option>
+            </select>
+        </label>
+
+    `}
+    else if(cantidadTotalProductos < 7 ){
+        document.querySelector("#facturacion").innerHTML=`
+        
+        <label>Cantidad de cuotas
+            <select>
+                <option>1 cuota </option>
+                <option>3 cuota </option>
+            </select>
+        </label>`
+    }
+    else if(cantidadTotalProductos >= 7 ){
+        document.querySelector("#facturacion").innerHTML=`
+        
+        <label>Cantidad de cuotas
+            <select>
+                <option>1 cuota </option>
+                <option>3 cuota </option>
+                <option>6 cuota </option>
+            </select>
+        </label>`
+    }
+
+    function cuotas(){
+
+    }
 }
